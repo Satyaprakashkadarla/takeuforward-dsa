0987372319b53a76a1d89d7e2889e7d0ff3a32b8
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-07-28 07:41:46 +0530

    Create BruteForce.java

---
+++ Arrays/Basic/002_Check_if_the_Array_is_Sorted/BruteForce.java
@@ -0,0 +1,27 @@
+/*
+Problem : Check if the Array is Sorted
+
+Approach : Compare Adjacent Elements
+
+Time Complexity : O(n)
+
+Space Complexity : O(1)
+
+Author : Satya Prakash Kadarla
+*/
+
+class Solution {
+
+    public boolean isSorted(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
