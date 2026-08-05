6155230e093b1911e486637a2fe34d6ee7d677c8
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-08-05 08:06:47 +0530

    Add next permutation solution in Optimal.java
    Implement next permutation algorithm with O(n) time complexity and O(1) space complexity.

---
+++ Arrays/Hard/002_Next_Permutation/Optimal.java
@@ -0,0 +1,62 @@
+/*
+Problem : Next Permutation
+
+Approach : Next Lexicographical Permutation
+
+Time Complexity : O(n)
+
+Space Complexity : O(1)
+
+Author : Satya Prakash Kadarla
+*/
+
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int n = nums.length;
+
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+
+        }
+
+        reverse(nums, i + 1, n - 1);
+
+    }
+
+    private void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+
+        while (left < right) {
+
+            swap(nums, left, right);
+
+            left++;
+            right--;
+
+        }
+
+    }
+
+}
